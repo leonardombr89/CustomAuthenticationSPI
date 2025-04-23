46018963a6dff9cee77f888fe798db5c160f2792
leonardo.barros <leonardombr@hotmail.com>
2025-04-23 09:26:01 -0300

    Add spi Authenticator

---
+++ src/main/java/org/conneqt/authenticator/MyAuthenticatorFactory.java
@@ -0,0 +1,76 @@
+package org.conneqt.authenticator;
+
+import org.keycloak.Config;
+import org.keycloak.authentication.Authenticator;
+import org.keycloak.authentication.AuthenticatorFactory;
+import org.keycloak.models.AuthenticationExecutionModel;
+import org.keycloak.models.KeycloakSession;
+import org.keycloak.models.KeycloakSessionFactory;
+import org.keycloak.provider.ProviderConfigProperty;
+
+import java.util.List;
+
+public class MyAuthenticatorFactory implements AuthenticatorFactory {
+
+    public static final String PROVIDER_ID = "authenticator-spi";
+
+    @Override
+    public String getDisplayType() {
+        return "Konneqt Custom Login";
+    }
+
+    @Override
+    public String getId() {
+        return PROVIDER_ID;
+    }
+
+    @Override
+    public String getReferenceCategory() {
+        return null;
+    }
+
+    @Override
+    public boolean isConfigurable() {
+        return false;
+    }
+
+    @Override
+    public AuthenticationExecutionModel.Requirement[] getRequirementChoices() {
+        return new AuthenticationExecutionModel.Requirement[]{
+                AuthenticationExecutionModel.Requirement.REQUIRED,
+                AuthenticationExecutionModel.Requirement.DISABLED
+        };
+    }
+
+    @Override
+    public boolean isUserSetupAllowed() {
+        return false;
+    }
+
+    @Override
+    public String getHelpText() {
+        return "Autentica via header X-Konneqt-Token";
+    }
+
+    @Override
+    public List<ProviderConfigProperty> getConfigProperties() {
+        return List.of();
+    }
+
+    @Override
+    public Authenticator create(KeycloakSession keycloakSession) {
+        return new MyAuthenticator(); // sua classe que implementa Authenticator
+    }
+
+    @Override
+    public void init(Config.Scope scope) {
+    }
+
+    @Override
+    public void postInit(KeycloakSessionFactory keycloakSessionFactory) {
+    }
+
+    @Override
+    public void close() {
+    }
+}
